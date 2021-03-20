32c48084908cebae5b307e86c459190055574815
Eric Bourne <ericbourne6371@gmail.com>
2021-03-20 16:31:09 -0700

    attached styling CDN's

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-// import Nav from "../components/nav";
+import Nav from "./components/nav";
 import Home from "./components/home";
 // import Footer from "../components/footer";
 // import Detail from "../components/detail";
@@ -15,7 +15,8 @@ function App() {
   return (
     <div className='App'>
       <Router>
-        <div>
+        <div className="wrapper">
+          <Nav />
           <Switch>
             <Route path='/' exact>
               <Home />
